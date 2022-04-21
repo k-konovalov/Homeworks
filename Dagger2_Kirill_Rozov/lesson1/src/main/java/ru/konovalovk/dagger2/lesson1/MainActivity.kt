0fa86fe998d521fcd772095809f8778621771f3b
Konovalov Kirill <k.konovalov@indriver.com>
2022-04-21 18:22:15 +0300

    Fill and inject basic data class

--- Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/MainActivity.kt
+++ Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/MainActivity.kt
@@ -3,11 +3,17 @@ package ru.konovalovk.dagger2.lesson1
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.konovalovk.dagger2.lesson1.data.Computer
+import ru.konovalovk.dagger2.lesson1.di.appComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    // ToDo: 3.5) Inject object from DI graph
+    @Inject lateinit var computer: Computer
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        0
+        // ToDo: 3.4) Inject AppComponent to Activity
+        appComponent.inject(this)
     }
 }
---
+++ Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/data/Computer.kt
@@ -0,0 +1,6 @@
+package ru.konovalovk.dagger2.lesson1.data
+
+//ToDo: 3.1) Create some data class
+data class Computer(
+    val test: String
+)
