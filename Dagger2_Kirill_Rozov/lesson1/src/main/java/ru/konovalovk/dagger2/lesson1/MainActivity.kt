2d82256a4ea13c7bb9efdffc22489e2dc1e5392a
Konovalov Kirill <k.konovalov@indriver.com>
2022-04-26 18:23:09 +0300

    Some Todo's

--- Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/MainActivity.kt
+++ Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/MainActivity.kt
@@ -3,18 +3,16 @@ package ru.konovalovk.dagger2.lesson1
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import ru.konovalovk.dagger2.lesson1.data.Computer
 import ru.konovalovk.dagger2.lesson1.di.appComponent
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     // ToDo: 3.5) Inject object from DI graph
-    @Inject lateinit var computer: Computer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // ToDo: 3.4) Inject AppComponent to Activity
         appComponent.inject(this)
-        findViewById<TextView>(R.id.tv_hello).text = computer.test
+        // ToDo: 3.5) Get computer from appComponent directly
+        findViewById<TextView>(R.id.tv_hello).text = appComponent.computer.test
     }
 }
--- Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/di/AppComponent.kt
+++ Dagger2_Kirill_Rozov/lesson1/src/main/java/ru/konovalovk/dagger2/lesson1/di/AppComponent.kt
@@ -2,11 +2,13 @@ package ru.konovalovk.dagger2.lesson1.di
 
 import dagger.Component
 import ru.konovalovk.dagger2.lesson1.MainActivity
+import ru.konovalovk.dagger2.lesson1.data.Computer
 
 // ToDo: 2.1) Create @Component interface
 // ToDo: 2.3) Specify new module there
 @Component(modules = [AppModule::class])
 interface AppComponent {
-    // ToDo: 2.4) Create fun, that inject component to activity
+    // ToDo: 2.4) Create val computer and fun, that inject component to activity
+    val computer: Computer
     fun inject(mainActivity: MainActivity)
 }
